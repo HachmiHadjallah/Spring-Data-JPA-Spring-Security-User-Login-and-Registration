3dced2daabbb83a48a3f19e67a412d3cd2dabfcf
Hachmi HADJALA <88934836+HachmiHadjallah@users.noreply.github.com>
2022-10-26 09:37:30 +0100

    Delete Login directory

--- Login/src/main/java/com/example/Login/appuser/AppUserRole.java
+++
@@ -1,6 +0,0 @@
-package com.example.Login.appuser;
-
-public enum AppUserRole {
-  USER,
-  ADMIN
-}
--- Login/src/main/java/com/example/Login/email/EmailSender.java
+++
@@ -1,7 +0,0 @@
-package com.example.Login.email;
-
-public interface EmailSender {
-	
-	void send(String to, String email);
-
-}
